254e48cc1d7e3eb037d53c25dc349c326a9d1c37
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-01-24 15:01:08 -0500

    fixed use window in production

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,12 +37,23 @@ export default function IndexPage(props) {
   const [stopFixed,setStopFixed] = useState(false);
 
   function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
 
-    return {
-      width,
-      height,
-    };
+    if( typeof window === "undefined" ){
+
+      const { innerWidth: width, innerHeight: height } = window;
+
+      return {
+        width,
+        height,
+      };
+
+    }else{
+      return {
+        width:0,
+        height:0,
+      }; 
+    }
+
   }
 
   const settingsAnimation = {
